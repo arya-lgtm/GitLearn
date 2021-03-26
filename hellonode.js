89220506c37d4f6610d1423cde55ac010de03d9b
Arya <arysheet@publicisgroupe.net>
2021-03-26 10:39:34 +0530

    push

--- hellonode.js
+++ hellonode.js
@@ -1,1 +1,15 @@
-console.log("arya");
+console.log("Hello Node !");
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 5505;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
